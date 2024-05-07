932a3f180363f92a1147630fe5fb0f67587e8256
Eduardo <25898378+sollericos@users.noreply.github.com>
2024-05-07 10:19:39 +0200

    Create listgrades.php
    Added "ca" language pack.

---
+++ lang/ca/listgrades.php
@@ -0,0 +1,73 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'listgrades', language 'ca', branch 'MOODLE_20_STABLE'
+ *
+ * @package    mod
+ * @subpackage listgrades
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['aepdmethod'] = 'Ocultació d\'identificadors AEPD';
+$string['aepdmethod_help'] = 'Fa servir el mètode d\'ocultació d\'identificadors que s\'emprarà per complir la normativa de l\'Agència Espanyola de Protecció de Dades: <a href="https://www.aepd.es/documento/orientaciones-da7.pdf">https://www.aepd.es/documento/orientaciones-da7.pdf</a>';
+$string['closedate'] = 'Fi de la publicació de les qualificacions';
+$string['closedate_help'] = 'Dia i hora en què es tancarà la llista.';
+$string['createlisting'] = 'Crea una llista';
+$string['defaultfooter'] = 'Text predeterminat del peu de pàgina';
+$string['defaultfooter_help'] = 'Text predeterminat del peu de pàgina que apareixerà al final de la llista.';
+$string['defaultfootertext'] = '<hr>
+ <p>Revisió de les qualificacions:</p>
+ <p>Data:</p>
+ <p>Hora:</p>
+ <p>Lloc:</p>
+ <hr>
+ <p><img class="img-fluid align-bottom" style="margin: 10px; float: left;" role="presentation" src="http://moodle3.local/moodle40/pluginfile.php/1/theme_moove/logo/1704277217/UVA_cuadro_rojo.jpg" alt="" width="64" height="64" align="left">Aquesta publicació té una finalitat informativa en l\'exercici de missions d\'interès públic previstes a la Llei orgànica del sistema universitari. El seu ús per part de l\'estudiant per a altres finalitats i, en particular, la seva alteració, manipulació i distribució indeguda a les xarxes socials o a altres mitjans públics pot generar responsabilitat jurídica.</p>';
+$string['defaultintro'] = 'Text predeterminat d\'introducció';
+$string['defaultintro_help'] = 'Text predeterminat d\'introducció que apareixerà a la llista.';
+$string['defaultintrotext'] = '<table border="0" width="100%"><tbody><tr><td><h3><img role="presentation" src="{$a->logourl}" alt="" width="121" height="78"></h3></td>
+ <td><h3>LLISTA DE QUALIFICACIONS PROVISIONALS</h3><p>Convocatòria: ORDINÀRIA</p></td>
+ </tr></tbody></table>';
+$string['footer'] = 'Secció del peu de pàgina<br>No modificar aquest contingut. Una vegada desat només es visualitzarà en un idioma.';
+$string['footerheader'] = 'Peu de pàgina';
+$string['gradeitems'] = 'Elements de qualificació per publicar.';
+$string['gradeitems_help'] = 'Selecciona els elements de qualificació que es publicaran. Manteniu pitjada la tecla CTRL per seleccionar diversos camps.';
+$string['listgrades:addinstance'] = 'Afegeix una llista de qualificacions';
+$string['listgrades:view'] = 'Mostra les llistes de qualificacions';
+$string['modulename'] = 'Publicació de qualificacions';
+$string['modulename_help'] = 'El mòdul "listgrades" permet a un professor publicar les qualificacions de tots els estudiants a una pàgina del curs per complir els requisits de transparència. Protegeix la privacitat dels estudiants ocultant el camp d\'usuari seleccionat pel professor.';
+$string['modulenameplural'] = 'Llista de qualificacions';
+$string['notopen'] = 'Aquesta llista encara no està disponible.';
+$string['opendate'] = 'Publicació de qualificacions';
+$string['opendate_help'] = 'Dia i hora en què es publicarà la llista.';
+$string['pluginadministration'] = 'Administració del mòdul "listgrades"';
+$string['pluginname'] = 'Publicació de qualificacions';
+$string['printintro'] = 'Mostra la descripció de l\'encapçalament';
+$string['privacy:metadata'] = 'El complement de recurs "listgrades" no emmagatzema cap dada personal.';
+$string['showuserfield'] = 'Mostra els identificadors d\'usuari';
+$string['showuserfield_help'] = 'Marca si apareixerà l\'ID de l\'usuari a la llista de qualificacions.';
+$string['showusername'] = 'Mostra els noms d\'usuari';
+$string['showusername_help'] = 'Marca si apareixerà el nom d\'usuari a la llista de qualificacions.';
+$string['userfield'] = 'Camp de l\'usuari';
+$string['userfield_help'] = 'Marca el camp de l\'usuari que es farà servir per mostrar la llista d\'usuaris.';
+$string['userfieldmask'] = 'Màscara del camp d\'usuari';
+$string['userfieldmask_help'] = 'Defineix la màscara que es farà servir per amagar el camp d\'usuari. Un * substituirà el caràcter; un - ometrà el caràcter, i un + mostrarà el caràcter.';
+$string['useridalways'] = 'Sempre';
+$string['useridnever'] = 'Mai';
+$string['useridonlyifnamecollide'] = 'Només si hi ha col·lisió de noms';
